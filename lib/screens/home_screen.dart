3f64290ceb4e521e2e67f474b9e762da54ae957e
valentin349 <valentino7007@gmail.com>
2023-02-02 18:32:20 +0000

    changed folder structure for authentication

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -45,54 +45,57 @@ class _HomeScreenState extends State<HomeScreen> {
                 size: 32,
               ),
               label: const Text('Sign out')),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 2,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedWorkout = _selectedWorkout == index ? -1 : index;
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    height: _selectedWorkout == index
-                        ? MediaQuery.of(context).size.height - kToolbarHeight
-                        : 100,
-                    child: Card(
-                      child: Column(
-                        children: <Widget>[
-                          ListTile(
-                            title: Text(workoutsProvider.workouts[index].name),
-                          ),
-                          _selectedWorkout == index
-                              ? Flexible(
-                                  child: ListView.builder(
-                                    itemCount:
-                                        exercisesProvider.exercises.length,
-                                    itemBuilder: (context, exerciseIndex) {
-                                      return ListTile(
-                                        title: Text(exercisesProvider
-                                            .exercises[exerciseIndex].name),
-                                        subtitle: Text(exercisesProvider
-                                            .exercises[exerciseIndex]
-                                            .description),
-                                      );
-                                    },
-                                  ),
-                                )
-                              : Container(),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          _createExpandBoxWidgets(workoutsProvider, exercisesProvider),
         ],
+      ),
+    );
+  }
+
+  Widget _createExpandBoxWidgets(
+      WorkoutsProvider workoutsProvider, ExercisesProvider exercisesProvider) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedWorkout = _selectedWorkout == index ? -1 : index;
+              });
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              height: _selectedWorkout == index
+                  ? MediaQuery.of(context).size.height - kToolbarHeight
+                  : 100,
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(workoutsProvider.workouts[index].name),
+                    ),
+                    _selectedWorkout == index
+                        ? Flexible(
+                            child: ListView.builder(
+                              itemCount: exercisesProvider.exercises.length,
+                              itemBuilder: (context, exerciseIndex) {
+                                return ListTile(
+                                  title: Text(exercisesProvider
+                                      .exercises[exerciseIndex].name),
+                                  subtitle: Text(exercisesProvider
+                                      .exercises[exerciseIndex].description),
+                                );
+                              },
+                            ),
+                          )
+                        : Container(),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
